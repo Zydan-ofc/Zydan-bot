ecfbf5b260213a521a0d2624c62e9fc5ddf5db96
Zydan-ofc <zidanefahrizi58@gmail.com>
2024-05-09 19:17:17 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,73 @@
+/* SC 𝖡𝖸 : 𝖣𝖨𝖬𝖹 𝖬𝖮𝖣𝖲
+𝖪𝖠𝖫𝖮 𝖬𝖠𝖴 𝖱𝖤𝖢𝖮𝖣𝖤, 𝖳𝖠𝖦 𝖦𝖴𝖠 𝖸𝖠𝖧
+𝖶𝖠 : 𝟢𝟪𝟥𝟪𝟩𝟣𝟪𝟧𝟧𝟤𝟣𝟥𝟧
+𝖨𝖦 : @dimz_𝗆𝗈𝖽𝗌
+𝖸𝖳 : 𝖽𝗂𝗆𝗓𝗆𝗈𝖽𝗌
+𝖦𝗋𝗈𝗎𝗉 : https://chat.whatsapp.com/GYr8yaZrnfvHxW020xM2Xb
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+global.owner = "6282267185830"
+global.namabot = "𝖣𝗂𝗆𝗓𝖬𝗈𝖽𝗌"
+global.botname = "𝖡𝗈𝗍 𝖣𝗂𝗆𝗆𝖺𝗓"
+global.autoJoin = false
+global.codeInvite = "FwtMxovJqW3Jj55x524hjT"
+global.thumb = fs.readFileSync("./thumb.png")
+global.sessionName = 'sesidimz' //Gausah Juga
+global.bugthomz = fs.readFileSync("./dimzbug.png")
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.packname = ""
+global.author = "𝖲𝗍𝗂𝗄𝖾𝗋 𝖻𝗒 𝖣𝗂𝗆𝗓𝗆𝗈𝖽𝗌"
+
+global.namastore = "KurBotz"
+global.nodana = "083871855213"
+global.qris = "https://telegra.ph/file/688235e772c878933edb7.jpg"
+
+global.domain = '' // Isi Domain Lu
+global.apikey = '' // Isi Apikey Plta Lu
+global.capikey = '' // Isi Apikey Pltc Lu
+global.eggsnya = '15' // id eggs yang dipakai
+global.location = '1' // id location
+
+global.antilink = false
+
+const mess = {
+   wait: "𝖶𝖺𝗂𝗍 𝗉𝗋𝗈𝗌𝖾𝗌 𝗆𝖺𝗌, 𝗁𝖺𝗋𝖺𝗉 𝗌𝖺𝖻𝖺𝗋...",
+   success: "𝖲𝗎𝗄𝗌𝖾𝗌 𝗆𝖺𝗌...",
+   save: "𝖲𝖺𝗏𝖾 𝖼𝗈𝗇𝗍𝖺𝖼𝗍 𝖻𝖾𝗋𝗁𝖺𝗌𝗂𝗅...",
+   on: "𝖮𝖭𝖫𝖨𝖭𝖤", 
+   off: "𝖮𝖥𝖥𝖫𝖨𝖭𝖤",
+   query: {
+       text: "Teks Nya Mana Kak?",
+       link: "Link Nya Mana Kak?",
+   },
+   error: {
+       fitur: "Mohon Maaf Kak Fitur Eror Silahkan Chat Developer Bot Agar Bisa Segera Diperbaiki",
+   },
+   only: {
+       group: "𝖬𝖺𝖺𝖿,𝖿𝗂𝗍𝗎𝗋 𝗂𝗇𝗂 𝗁𝖺𝗇𝗒𝖺 𝖽𝖺𝗉𝖺𝗍 𝖽𝗂𝗀𝗎𝗇𝖺𝗄𝖺𝗇 𝖽𝗂𝖽𝖺𝗅𝖺𝗆 𝗀𝗋𝗈𝗎𝗉...",
+       private: " 𝖬𝖺𝖺𝖿,𝖿𝗂𝗍𝗎𝗋 𝗂𝗇𝗂 𝗁𝖺𝗇𝗒𝖺 𝖽𝖺𝗉𝖺𝗍 𝖽𝗂𝗀𝗎𝗇𝖺𝗄𝖺𝗇 𝖽𝗂𝗉𝗋𝗂𝗏𝖺𝗍𝖾 𝖼𝗁𝖾𝗍...",
+       owner: "𝖬𝖺𝖺𝖿 𝖿𝗂𝗍𝗎𝗋 𝗂𝗇𝗂 𝗄𝗁𝗎𝗌𝗎𝗌 𝗈𝗐𝗇𝖾𝗋 𝗌𝖺𝗃𝖺...",
+       admin: "𝖬𝖺𝖺𝖿 𝖿𝗂𝗍𝗎𝗋 𝗂𝗇𝗂 𝗄𝗁𝗎𝗌𝗎𝗌 𝖺𝖽𝗆𝗂𝗇 𝖽𝗂𝗆𝗓𝗆𝗈𝖽𝗌...",
+       badmin: "𝖬𝖺𝖺𝖿,𝖿𝗂𝗍𝗎𝗋 𝗂𝗇𝗂 𝗍𝗂𝖽𝖺𝗄 𝖽𝖺𝗉𝖺𝗍 𝖽𝗂𝗀𝗎𝗇𝖺𝗄𝖺𝗇 𝖽𝗂𝗄𝖺𝗋𝖾𝗇𝖺𝗄𝖺𝗇 𝖻𝗈𝗍 𝖻𝗎𝗄𝖺𝗇 𝖺𝖽𝗆𝗂𝗇...",
+       premium: "𝖬𝖺𝖺𝖿,𝗄𝖺𝗆𝗎 𝖻𝗎𝗄𝖺𝗇 𝗎𝗌𝖾𝗋 𝗉𝗋𝖾𝗆𝗂𝗎𝗆,𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗆𝖻𝖾𝗅𝗂 𝖺𝗄𝗌𝖾𝗌 𝗌𝗂𝗅𝖺𝗁𝗄𝖺𝗇 𝗄𝖾𝗍𝗂𝗄 .𝗈𝗐𝗇𝖾𝗋 𝗎𝗇𝗍𝗎𝗄 𝗆𝖾𝗇𝗀𝗁𝗎𝖻𝗎𝗇𝗀𝗂 𝗈𝗐𝗇𝖾𝗋",
+   }
+}
+
+global.mess = mess
+//=================================================//
+//Gausah Juga
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
